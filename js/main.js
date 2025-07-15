7c64aa5be0cf256f79d8da9602c086c56ee1919b
ayuri <ringoayu713@gmail.com>
2025-07-15 21:58:46 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+  /*=================================================
+  ハンバーガーメニュー
+  ===================================================*/
+  $(".toggle-btn, .mask, nav a").on("click", function () {
+    $("header").toggleClass("open");
+  });
+
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 3000, "swing");
+    return false;
+  });
+
+
+
+  // /*=================================================
+  // スクロール時の画像フェード表示
+  // ===================================================*/
+  // スクロール時のイベント
+  $(window).scroll(function () {
+    // fadeinクラスに対して順に処理を行う
+    $(".fadein").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+  });
+
+
+});
